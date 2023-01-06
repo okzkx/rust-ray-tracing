c4a0dbc80c3b7a3355b55ee2651c568fb9b6ad90
ZKX <zengkaixiang@xmfunny.com>
2023-01-06 14:56:21 +0800

    Adding a Progress Indicator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // Ray Tracing in One Weekend
 // https://misterdanb.github.io/raytracinginrust/#overview
 
+use std::io::{stderr, Write};
+
 fn main() {
     const IMAGE_WIDTH: u64 = 256;
     const IMAGE_HEIGHT: u64 = 256;
@@ -10,6 +12,9 @@ fn main() {
     println!("255");
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {:3}", IMAGE_HEIGHT - j - 1);
+        stderr().flush().unwrap();
+
         for i in 0..IMAGE_WIDTH {
             let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
             let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
@@ -22,5 +27,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+    eprintln!("Done.");
 }
 
