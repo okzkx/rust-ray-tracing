d41495965c6c00c7222fef1eeafaca95c7e37250
ZKX <zengkaixiang@xmfunny.com>
2023-02-22 10:56:27 +0800

    Diffuse Materials

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,16 @@ use hit::{Hit, World};
 use sphere::Sphere;
 use camera::Camera;
 
-fn ray_color(r: &Ray, world: &World) -> Color {
-    if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
-        0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0))
+fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
+    if depth <= 0 {
+        // If we've exceeded the ray bounce limit, no more light is gathered
+        return Color::new(0.0, 0.0, 0.0);
+    }
+
+    if let Some(rec) = world.hit(r, 0.001, f64::INFINITY) {
+        let target = rec.p + Vec3::random_in_hemisphere(rec.normal);
+        let r = Ray::new(rec.p, target - rec.p);
+        0.5 * ray_color(&r, world, depth - 1)
     } else {
         let unit_direction = r.direction().normalized();
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -26,12 +33,12 @@ fn ray_color(r: &Ray, world: &World) -> Color {
 }
 
 fn main() {
-
     // Image
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u64 = 256;
     const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
-    const SAMPLES_PER_PIXEL: u64 = 100;
+    const SAMPLES_PER_PIXEL: u64 = 10;
+    const MAX_DEPTH: u64 = 5;
 
     // World
     let mut world = World::new();
@@ -47,7 +54,7 @@ fn main() {
 
     let mut rng = rand::thread_rng();
     for j in (0..IMAGE_HEIGHT).rev() {
-        eprint!("\rScan lines remaining: {:3}", IMAGE_HEIGHT - j - 1);
+        eprint!("\rScanlines remaining: {:3}", IMAGE_HEIGHT - j - 1);
         stderr().flush().unwrap();
 
         for i in 0..IMAGE_WIDTH {
@@ -60,11 +67,12 @@ fn main() {
                 let v = ((j as f64) + random_v) / ((IMAGE_HEIGHT - 1) as f64);
 
                 let r = cam.get_ray(u, v);
-                pixel_color += ray_color(&r, &world);
+                pixel_color += ray_color(&r, &world, MAX_DEPTH);
             }
 
             println!("{}", pixel_color.format_color(SAMPLES_PER_PIXEL));
         }
     }
+
     eprintln!("Done.");
 }
